184b35fe0af65c5f2a1267a520dd704bce9defa1
Tung Nguyen <tongueroo@gmail.com>
2018-07-12 15:33:46 -0700

    update demo app

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World Hello.java";
+      message = "Hello World: src/main/java/Hello.java";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World index.jsp!</h2>
+<h2>Hello World: src/main/webapp/index.jsp</h2>
 </body>
 </html>
