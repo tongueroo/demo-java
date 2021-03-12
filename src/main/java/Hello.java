c5136f295f7623eb7199d3fb9f8f67bd39e8ded1
Manoj Indalkar <73096275+mnidevops@users.noreply.github.com>
2021-03-12 16:17:40 +0530

    Update Hello.java
    Updated file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -10,7 +10,7 @@ public class Hello extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World: src/main/java/Hello.java";
+      message = "Hello World: Welcome to MNIDevops Technologies Ltd.";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
